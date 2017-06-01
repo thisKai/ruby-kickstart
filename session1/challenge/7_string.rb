fab1519ef64f8d61b2ae967b7c2620755c3dccaa
Kai <thiskaiguy@gmail.com>
2017-06-01 23:09:23 +0100

    Completed session 1 challenge 7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,17 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  out_string = ""
+  prev_char = ""
+  string.each_char do |char|
+    if prev_char == "r" || prev_char == "R"
+      out_string << char
+    end
+    prev_char = char
+  end
+  out_string
 end
