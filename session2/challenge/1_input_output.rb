0f0fe3ecc9a2c67818b5bb4b8f37450008f2326e
Kai <thiskaiguy@gmail.com>
2017-06-04 21:36:56 +0100

    Complete session 2 challenge 1

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -3,10 +3,20 @@
 
 # Write a program that reads in two integers typed on the keybaord
 # and outputs their sum, difference, and product
-# 
+#
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
   # your code goes here
+  input = gets.chomp.split(' ')
+  n1 = input[0].to_i
+  n2 = input[1].to_i
+
+  # sum
+  puts n1 + n2
+  # difference
+  puts n1 - n2
+  #product
+  puts n1 * n2
 end
