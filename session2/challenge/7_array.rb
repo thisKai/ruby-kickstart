ef1b7187e03f4055f2c7fd48e46c350f44853b68
Kai <thiskaiguy@gmail.com>
2017-06-05 00:02:19 +0100

    Completed session 2 challenge 7

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -3,8 +3,16 @@
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
 # There will be no "'" that is not part of a contraction.
 # Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
-# 
+#
 # Examples
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+Punctuation = /[!@$#%^&*\(\)-=_+\[\]:;,\.\/<>?\|]/
+def alternate_words(phrase)
+  result = []
+  phrase.gsub(Punctuation, ' ').split(/[\s]+/).each_slice(2) do |a, b|
+    result << a
+  end
+  result
+end
