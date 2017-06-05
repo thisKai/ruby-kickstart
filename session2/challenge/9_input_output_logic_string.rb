20dcf85dce50379b167974008726b6e7c7417ddd
Kai <thiskaiguy@gmail.com>
2017-06-05 20:31:55 +0100

    Complete session 2 challenge 9

--- session2/challenge/9_input_output_logic_string.rb
+++ session2/challenge/9_input_output_logic_string.rb
@@ -24,7 +24,15 @@
 # USER:     BYE
 
 def deaf_grandma
-
+  while line = gets
+    input = line.chomp
+    break if input == "BYE"
+    if input.length > 0 && input == input.upcase
+      puts "NO, NOT SINCE 1938!"
+    else
+      puts "HUH?! SPEAK UP, SONNY!"
+    end
+  end
 end
 
 
