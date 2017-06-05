c32c40286b804f797f69d753445dcbacae9ff11a
Kai <thiskaiguy@gmail.com>
2017-06-05 23:28:35 +0100

    Complete session 2 challenge 13

--- session2/challenge/13_classes_iterating_sorting.rb
+++ session2/challenge/13_classes_iterating_sorting.rb
@@ -64,3 +64,45 @@
 # date docs are at: http://ruby-doc.org/core/classes/Date.html
 # don't spend too much time worrying about them :)
 require 'date'
+
+class User
+  attr_accessor :username
+  def initialize(username)
+    @username = username
+    @blogs = []
+  end
+
+  def add_blog(date, text)
+    blog = Blog.new(date, self, text)
+    @blogs << blog
+    blog
+  end
+
+  def blogs
+    @blogs.sort_by { |blog| blog.date }.reverse
+  end
+end
+
+class Blog
+  attr_accessor :text, :date, :user
+  def initialize(date, user, text)
+    @date = date
+    @user = user
+    @text = text
+  end
+
+  def summary
+    @text.split(' ').take(10).join(' ')
+  end
+
+  def entry
+    "#{@user.username} #{@date.to_s}\n#{@text}"
+  end
+
+  def ==(other)
+    return false if !other.is_a? Blog
+    self.date == other.date &&
+      self.user == other.user &&
+      self.text == other.text
+  end
+end
