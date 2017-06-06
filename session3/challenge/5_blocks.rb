f6c2d3c0023e84af1f7a63039848a2e42fd5d188
Kai <thiskaiguy@gmail.com>
2017-06-07 00:52:04 +0100

    Complete session 3 challenge 5

--- session3/challenge/5_blocks.rb
+++ session3/challenge/5_blocks.rb
@@ -16,3 +16,36 @@
 #   order << i
 # end
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+
+def spiral_access(square_array, &block)
+  return if square_array.empty? || square_array.first.empty?
+
+  ubound = square_array.length - 1
+
+  # call block with items in the first row
+  square_array.first.each do |i|
+    block.call i
+  end
+  if ubound > 0
+    # call block with last item of every subsequent row
+    square_array[1..ubound].each do |row|
+      block.call row[ubound]
+    end
+    # call block with items in the last row backwards
+    square_array.last[0..ubound-1].reverse.each do |i|
+      block.call i
+    end
+    # call block with items in the first column backwards, except fot the first item
+    square_array[1..ubound-1].reverse.each do |row|
+      block.call row.first
+    end
+  end
+  new_square = square_array[1..square_array.length-2].map do |row|
+    row[1..row.length-2]
+  end
+
+  if !new_square.empty?
+    # call this function again on the inner square
+    spiral_access new_square, &block
+  end
+end
